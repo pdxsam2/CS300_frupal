b5e98e992bb22ac447d7d236d0ea9e334920d53b
Timothy hall <59980086+halltim91@users.noreply.github.com>
2020-03-11 19:23:07 -0700

    Updated Config
    updated config to save changes made to new config menu
    updated config to serialize game and player settings

--- main.py
+++ main.py
@@ -35,6 +35,9 @@ class GameState:
 	entity_manifest = []
 	entities = []
 	total_entity_chance = 0
+	x_dim= 25
+	y_dim= 35
+	intro_flag= 0
 
 screenManager = screenManager()
 
@@ -56,7 +59,7 @@ def init():
 
 	loadConfig(state)
 	state.total_entity_chance = 0
-	
+
 	add_entity(state, "Magic Jewel", 0.0)
 	add_entity(state, "Greedy Tile", 0.0075)
 
