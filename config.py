43737a7c32c400be348ac9e13ca5ff85d0982b0e
JECoyle <Zilarrezko76@gmail.com>
2020-03-12 01:44:29 -0700

    Comment out debug printout, and fixing a warning message

--- config.py
+++ config.py
@@ -33,8 +33,8 @@ def loadConfig(state):
 	inv_slot = 0	#Note(Austin): I added a field in item to help me find the respective array index for user.inv[]
 	for i in items:
 		data = i.split(",")
-		print("Data: " + data[0] + " " + data[1] + " " + data[2] + " " + data[3])
-		add_item(state, data[0], int(data[1]), int(data[2]), inv_slot, True if data[3] is 1 else False)
+		# Debug: print("Data: " + data[0] + " " + data[1] + " " + data[2] + " " + data[3])
+		add_item(state, data[0], int(data[1]), int(data[2]), inv_slot, True if data[3] == 1 else False)
 		inv_slot += 1
 	
 	#load settings
